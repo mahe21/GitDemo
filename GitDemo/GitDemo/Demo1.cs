7b7ce8206e58ecbaae0097829d167ec8170e9f72
Mahesh <Maheshk2104@gmail.com>
2023-06-14 09:29:44 +0530

    [Mahesh]Add: Initial commit

---
+++ GitDemo/GitDemo/Demo1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    internal class Demo1
+    {
+        public void Demo()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ GitDemo/GitDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GitDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Demo1 demo1 = new Demo1();
+            demo1.Demo();
+            Console.ReadLine();
+            
+        }
+    }
+}
